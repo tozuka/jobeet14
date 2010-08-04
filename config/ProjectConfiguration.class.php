f62d70ce594c5227b021a2906c5d33bf7522ab91
tozuka <tozuka@tejimaya.com>
2010-08-04 13:37:32 +0900

    la fin du jour 01; j'ai fait le frontend app avec succes.

---
+++ config/ProjectConfiguration.class.php
@@ -0,0 +1,12 @@
+<?php
+
+require_once dirname(__FILE__).'/../lib/vendor/RELEASE_1_4_0/lib/autoload/sfCoreAutoload.class.php';
+sfCoreAutoload::register();
+
+class ProjectConfiguration extends sfProjectConfiguration
+{
+  public function setup()
+  {
+    $this->enablePlugins('sfDoctrinePlugin');
+  }
+}
